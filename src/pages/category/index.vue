2878e385d0bb159b602764f94542846db90af8e5
sharkwarn <sharkwarn@163.com>
2018-10-07 10:24:55 +0800

    category

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="counter-warp">
+  <div class="container">
     
   </div>
 </template>
@@ -11,6 +11,6 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less" scoped>
 
 </style>
